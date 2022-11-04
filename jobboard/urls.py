2e07d470a33f218344822ef61c45cc465ca129af
Martinovdima <martinovdima@rambler.ru>
2022-11-05 01:22:46 +0600

    added transcribation

--- jobboard/urls.py
+++ jobboard/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from services.views import index
+import services.views as services
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', services.index, name='index'),
+    path('transcribation/', services.transcrib, name='transcrib')
 ]
--- services/views.py
+++ services/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'services/index.html')
+    return render(request, 'services/index.html')
+
+def transcrib(request):
+    return render(request, 'services/transcribation.html')
