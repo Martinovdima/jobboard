4e4f9c7595b08ab8e2ad2a8633b28de60f5ddff8
Martinovdima <martinovdima@rambler.ru>
2022-11-08 16:11:31 +0600

    unknow

--- services/views.py
+++ services/views.py
@@ -1,39 +1,38 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    title = 'Главная'
     content = {
-        'title': title
+        'title': 'Главная',
     }
-
     return render(request, 'services/index.html', content)
 
+
 def transcrib(request):
-    title = 'Транскрибация'
     content = {
-        'title': title
+        'title': 'Транскрибация'
     }
     return render(request, 'services/transcribation.html', content)
 
+
 def shop(request):
-    title = 'Интернет-магазин'
     content = {
-        'title': title
+        'title': 'Интернет-магазин',
     }
     return render(request, 'services/shop.html', content)
 
+
 def parsing(request):
-    title = 'Парсинг'
     content = {
-        'title': title
+        'title': 'Парсинг',
     }
     return render(request, 'services/parsing.html', content)
 
+
 def telegramm(request):
-    title = 'Telegramm бот'
     content = {
-        'title': title
+        'title': 'Telegramm бот',
     }
     return render(request, 'services/telegramm.html', content)
