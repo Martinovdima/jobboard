f72bf1da99940b7e2075eb1c816df43273eecef5
Martinovdima <martinovdima@rambler.ru>
2022-11-13 19:41:36 +0600

    added usersapp

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
-    'services'
+    'services',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
