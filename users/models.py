3c9ac856183fddd3143a114ecc4776dcf3134775
Martinovdima <martinovdima@rambler.ru>
2022-11-13 23:53:47 +0600

    added user templates

--- users/models.py
+++ users/models.py
@@ -4,4 +4,3 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
-    age = models.PositiveIntegerField(verbose_name='age')
