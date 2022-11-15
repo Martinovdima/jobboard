ee3a2cf14c77f8bd0196dcc17505c07b7c0622b7
Martinovdima <martinovdima@rambler.ru>
2022-11-15 15:04:27 +0600

    added login and register templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    content = {'title': 'Login'}
+    return render(request, 'users/login.html', content)
+
+def register(request):
+    content = {'title': 'Registration'}
+    return render(request, 'users/register.html', content)
