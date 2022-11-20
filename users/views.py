41542aa782fd91e70071503b6a88739a7bfee135
kostgr <kostgr87@yandex.ru>
2022-11-20 13:07:16 +0600

    added forms logic login

--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 # Create your views here.
 def login(request):
-    content = {'title': 'Login'}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    content = {'title': 'Login', 'form': form}
     return render(request, 'users/login.html', content)
 
 def register(request):
